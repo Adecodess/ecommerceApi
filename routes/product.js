f321e021d5a2ce0cde07b0ab8f0d794004b9d6da
adecodess <ademosuu@gmail.com>
2021-10-14 21:58:48 +0100

    completed product router

--- routes/product.js
+++ routes/product.js
@@ -1,8 +1,89 @@
 //  require router
 const router = require('express').Router();
 
-// get request to /user
+const Product = require('../models/Product');
 
-// post request to /user
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require('../middleware/verifyToken');
+
+// create product
+router.post('/', verifyTokenAndAdmin, async (req, res) => {
+  const newProduct = new Product(req.body);
+  try {
+    const savedProduct = await newProduct.save();
+    res.status(201).send(savedProduct);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// put request to update product
+router.put('/:id', verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedProduct = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+
+      { new: true }
+    );
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+// // delete request to delete product
+router.delete('/:id', verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json('product deleted successfully....');
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+// get request to fetch single product
+router.get('/find/:id', async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!user) {
+      res.status(401).json('product not found');
+    }
+
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+// // get request to fetch all product
+router.get('/', async (req, res) => {
+  const qNew = req.query.new;
+  const qCategory = req.query.category;
+  try {
+    let products;
+    if (qNew) {
+      products = await Product.find().sort({ createdAt: -1 }).limit(5);
+    } else if (qCategory) {
+      products = await Product.find({ categories: { $in: [qCategory] } });
+    } else {
+      products = await Product.find();
+    }
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
